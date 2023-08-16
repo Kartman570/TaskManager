e531cf43e2b867de6d0cc0f5d347b2b3155f4486
Kartman570 <kotelnikovilya@yandex.ru>
2023-08-16 08:15:05 +0400

    fix permissions and add test for it

--- main/views.py
+++ main/views.py
@@ -13,14 +13,14 @@ class UserFilter(django_filters.FilterSet):
         fields = ("name",)
 
 
-class TaskViewSet(viewsets.ModelViewSet):
+class TaskFilter(django_filters.FilterSet):
     state = django_filters.CharFilter(lookup_expr="icontains")
     tags = django_filters.CharFilter(lookup_expr="icontains")
     worker = django_filters.CharFilter(lookup_expr="icontains")
     author = django_filters.CharFilter(lookup_expr="icontains")
 
     class Meta:
-        model = Tag
+        model = Task
         fields = ("state", "tags", "worker", "author")
 
 
@@ -34,6 +34,7 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.order_by("id")
     serializer_class = TaskSerializer
     permission_classes = [IsStaffOrReadOnly]
+    filterset_class = TaskFilter
 
 
 class TagViewSet(viewsets.ModelViewSet):
