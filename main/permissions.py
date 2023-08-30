2d2010e0910e71fd7d899580e9060298881e7b48
Kartman570 <kotelnikovilya@yandex.ru>
2023-08-30 14:12:38 +0400

    fix is_staff and has_permissions

--- main/permissions.py
+++ main/permissions.py
@@ -2,7 +2,8 @@ from rest_framework import permissions
 
 
 class IsStaffOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+    def has_permission(self, request, view):
+        if request.method == "DELETE":
+            return request.user.is_staff
+        else:
             return True
-        return request.user.is_staff
