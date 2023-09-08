68f7d91f0847f7c28066613b927dcc7bd9afa084
Kartman570 <kotelnikovilya@yandex.ru>
2023-09-08 20:59:13 +0400

    update IsStaffOrReadOnly

--- main/permissions.py
+++ main/permissions.py
@@ -3,7 +3,6 @@ from rest_framework import permissions
 
 class IsStaffOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method == "DELETE":
-            return request.user.is_staff
-        else:
+        if request.method != "DELETE":
             return True
+        return request.user.is_staff
