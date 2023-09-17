50a16078e728fe609da5ed8ffe6808f7a19585b5
Kartman570 <kotelnikovilya@yandex.ru>
2023-09-17 20:07:15 +0400

    IsAuthenticated was already done in prev branches

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -134,5 +134,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "main.User"
 REST_FRAMEWORK = {
 "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
-'DEFAULT_PERMISSION_CLASSES': ("rest_framework.permissions.IsAuthenticated",),
+'DEFAULT_PERMISSION_CLASSES': ("rest_framework.permissions.IsAuthenticated",), #ALREADY DONE
 }
