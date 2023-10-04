da5ce6aba61cf0ef18128ea39b9edc61f110732e
Kartman570 <kotelnikovilya@yandex.ru>
2023-10-05 02:41:08 +0400

    serialize + tmp fix on user test

---
+++ main/services/file_validator.py
@@ -0,0 +1,9 @@
+from django.core.files.base import File
+
+class FileMaxSizeValidator:
+    def __init__(self, max_size: int) -> None:
+        self.max_size = max_size
+
+    def __call__(self, value: File) -> None:
+        if value.size > self.max_size:
+            raise ValidationError(f"Maximum size {self.max_size} exceeded.")
